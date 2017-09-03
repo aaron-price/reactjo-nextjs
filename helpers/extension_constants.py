b1473b788f19ec90004771b20577c73653a33355
Aaron Price <coding.aaronp@gmail.com>
2017-09-03 15:43:52 -0700

    Update rc_home

--- helpers/extension_constants.py
+++ helpers/extension_constants.py
@@ -2,5 +2,5 @@
 # file_manager('$out/some/path') == 'super_root/project_path/frontend/some/path'
 OUTPUT_HOME = 'frontend'
 
-# file_manager('$rc/assets') == 'super_root/reactjorc/extensions/nextjs_trial/assets'
-RC_HOME = 'nextjs_trial'
+# file_manager('$rc/assets') == 'super_root/reactjorc/extensions/reactjo_nextjs/assets'
+RC_HOME = 'reactjo_nextjs'
