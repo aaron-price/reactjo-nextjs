34e454b092d2881b85c7e37a3177d32950196247
Aaron Price <coding.aaronp@gmail.com>
2017-09-13 11:18:59 -0700

    Force quotes around random signature

--- scaffolding/main_structure.py
+++ scaffolding/main_structure.py
@@ -5,7 +5,8 @@ import os, subprocess
 import string
 import random
 def id_generator(size=16, chars=string.ascii_uppercase + string.digits):
-    return ''.join(random.choice(chars) for _ in range(size))
+    signature = ''.join(random.choice(chars) for _ in range(size))
+    return "'{signature}'"
 
 def build_structure():
     prev_path = os.getcwd()
