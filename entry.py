a3331209c7ca4a3b3246d6850445509a90eb60b8
Aaron Price <coding.aaronp@gmail.com>
2017-09-15 13:46:08 -0700

    Refactor build command

--- entry.py
+++ entry.py
@@ -1,11 +1,11 @@
 import sys, os, subprocess
 
 from commands.new import new
-from commands.scaffold import scaffold
+from commands.build import build
 from helpers.file_manager import file_manager as f
 
 cmd = sys.argv[1]
 if cmd in ['n', 'new']:
 	new()
 if cmd in ['b', 'build']:
-	scaffold()
+	build()
--- scaffolding/content_pages.py
+++ scaffolding/content_pages.py
@@ -3,7 +3,6 @@ import os, subprocess
 from inflection import pluralize
 
 from helpers.config_manager import get_cfg
-from helpers.ui import boolean_input
 from helpers.file_manager import file_manager as f
 
 def quote(string):
@@ -55,20 +54,3 @@ def scaffold_details_page():
 
     f('$pages/' + title_singular + '.js', 'w', details_page)
     print('Built the details page!')
-
-def scaffold_menu_item():
-    cfg = get_cfg()
-    title = cfg['current_scaffold']['model']['title']
-    title_plural = pluralize(title.capitalize())
-    data = {
-        'target': ['const content_types'],
-        'content': f"\n    '{title_plural}',"
-    }
-    f('$out/components/Navbar.js', 'a', data)
-
-def scaffold():
-    if boolean_input('Do you need a list page?', 'y'):
-        scaffold_list_page()
-        scaffold_menu_item()
-    if boolean_input('Do you need a details page?', 'y'):
-        scaffold_details_page()
