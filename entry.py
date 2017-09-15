b1d29ac46891157dc82125a901ab9654e0c8671c
Aaron Price <coding.aaronp@gmail.com>
2017-09-15 12:24:09 -0700

    Add reactjo serve command

--- entry.py
+++ entry.py
@@ -1,9 +1,14 @@
+import sys, os, subprocess
+
 from commands.new import new
 from commands.scaffold import scaffold
-import sys
+from helpers.file_manager import file_manager as f
 
 cmd = sys.argv[1]
 if cmd in ['n', 'new']:
 	new()
 if cmd in ['s', 'scaffold']:
 	scaffold()
+if cmd in ['server', 'serve']:
+	os.chdir(f('$out', '$'))
+	subprocess.Popen(['npm', 'run', 'dev'])
