9b3f80b687a841f0841d54630649a9a0905d2c1f
Aaron Price <coding.aaronp@gmail.com>
2017-09-15 14:50:52 -0700

    Separate everything that needs user auth

--- assets/components/Navbar.js
+++ assets/components/Navbar.js
@@ -15,38 +15,9 @@ const HomeLink = props => (
 		</Link>
 )
 
-// User Auth
-const UserLink = props => (
-    <Link href={{
-          pathname: '/user/',
-          asPath: `/user/`,
-          query: { id: props.id }
-    }}>
-				<a style={linkStyle}>Hi, {props.name}!</a>
-		</Link>
-)
-
-const LoginLink = props => (
-		<Link href="/login">
-				<RaisedButton style={linkStyle}>Login</RaisedButton>
-		</Link>
-)
-const LogoutLink = props => (
-		<RaisedButton style={linkStyle} onClick={() => props.logout()}
-    >Logout</RaisedButton>
-)
-
-const SignupLink = props => (
-		<Link href="/signup">
-				<RaisedButton style={linkStyle}>Signup</RaisedButton>
-		</Link>
-)
-
 // Content
-const content_types = [
-    'Users',
-]
-const ContentLinks = props => {    
+const content_types = []
+const ContentLinks = props => {
     return (
       <span>
           {content_types.map((item, key) => {
@@ -61,52 +32,11 @@ const ContentLinks = props => {
     )
 }
 
-class Navbar extends React.Component {
-    constructor(props){
-        super(props)
-        this.state = {
-            name: this.props.current_user.name,
-            id: this.props.current_user.id
-        }
-        this.update_user = this.update_user.bind(this)
-        this.logout = this.logout.bind(this)
-    }
-    update_user(current_user) {
-        this.setState({
-            name: current_user.name,
-            id: current_user.id
-        })
-    }
-    logout() {
-        fetch('/logout', {
-            method: 'POST',
-            credentials: 'include'
-        })
-        .then(data => {
-            this.update_user({ id: null, name: null })
-            Router.push({pathname: '/', as: '/'})
-        })
-        .catch(e => console.error(e))
-    }
-    render() {
-        return this.state.id && this.state.name
-        ? (
-            <div>
-                <UserLink id={this.state.id} name={this.state.name} />
-                <LogoutLink logout={this.logout} />
-                <br/><br/>
-                <HomeLink />
-                <ContentLinks />
-            </div>
-        ) : (
-            <div>
-                <HomeLink />
-                <LoginLink />
-                <SignupLink />
-                <ContentLinks />
-            </div>
-        )
-    }
-}
+const Navbar = (props) => (
+    <div>
+        <HomeLink />
+        <ContentLinks />
+    </div>
+)
 
 export default Navbar
--- scaffolding/main_structure.py
+++ scaffolding/main_structure.py
@@ -14,59 +14,71 @@ def id_generator():
 def build_structure():
     prev_path = os.getcwd()
     cfg = get_cfg()
+    users = cfg['need_users'] == 'True'
     project_name = cfg['project_name']
 
     # directories
     mkdir('$out')
     mkdir('$out/components')
-    mkdir('$out/middleware')
+    if users:
+        mkdir('$out/middleware')
     mkdir('$out/pages')
-    mkdir('$out/redux')
     mkdir('$out/services')
     mkdir('$out/styles')
 
     # Component assets
     f('$out/components/Head.js', 'w', '$assets/components/Head.js')
-    f('$out/components/Navbar.js', 'w', '$assets/components/Navbar.js')
+    if users:
+        f('$out/components/Navbar.js', 'w', '$assets/components/Navbar_users.js')
+    else:
+        f('$out/components/Navbar.js', 'w', '$assets/components/Navbar.js')
 
     # Middleware
-    res_current_user = f('$assets/middleware/res_current_user.js', 'r').replace(
-        'reactjo', project_name)
-    f('$out/middleware/res_current_user.js', 'w', res_current_user)
+    if users:
+        res_current_user = f('$assets/middleware/res_current_user.js', 'r').replace(
+            'reactjo', project_name)
+        f('$out/middleware/res_current_user.js', 'w', res_current_user)
 
     # Misc assets
     f('$out/package.json', 'w', '$assets/package.js')
 
-    server = f('$assets/server.js', 'r').replace(
+    if users:
+        server_string = f('$assets/server_users.js', 'r')
+    else:
+        server_string = f('$assets/server.js', 'r')
+
+    server_string = server_string.replace(
         'random_string', id_generator()).replace(
         'reactjo', project_name)
-    f('$out/server.js', 'w', server)
+
+    f('$out/server.js', 'w', server_string)
 
     # Pages assets
     f('$out/pages/index.js', 'w', '$assets/pages/index.js')
-    f('$out/pages/login.js', 'w', '$assets/pages/login.js')
-    f('$out/pages/signup.js', 'w', '$assets/pages/signup.js')
-    f('$out/pages/user.js', 'w', '$assets/pages/user.js')
-    f('$out/pages/users.js', 'w', '$assets/pages/users.js')
 
-    # Redux assets
-    # @TODO
+    if users:
+        f('$out/pages/login.js', 'w', '$assets/pages/login.js')
+        f('$out/pages/signup.js', 'w', '$assets/pages/signup.js')
+        f('$out/pages/user.js', 'w', '$assets/pages/user.js')
+        f('$out/pages/users.js', 'w', '$assets/pages/users.js')
 
     # Services assets
-    login_service = f('$assets/services/login_service.js', 'r').replace(
-        'reactjo', project_name)
-    f('$out/services/login_service.js', 'w', login_service)
-    f('$out/services/signup_service.js', 'w', '$assets/services/signup_service.js')
     f('$out/services/content_create.js', 'w', '$assets/services/content_create.js')
     f('$out/services/content_update.js', 'w', '$assets/services/content_update.js')
     f('$out/services/content_delete.js', 'w', '$assets/services/content_delete.js')
-    current_user_service = f('$assets/services/current_user.js', 'r').replace(
-        'reactjo', project_name)
-    f('$out/services/current_user.js', 'w', current_user_service)
+
+    if users:
+        login_service = f('$assets/services/login_service.js', 'r').replace(
+            'reactjo', project_name)
+        f('$out/services/login_service.js', 'w', login_service)
+        f('$out/services/signup_service.js', 'w', '$assets/services/signup_service.js')
+        current_user_service = f('$assets/services/current_user.js', 'r').replace(
+            'reactjo', project_name)
+        f('$out/services/current_user.js', 'w', current_user_service)
 
     # Style assets
     f('$out/styles/styles.js', 'w', '$assets/styles/styles.js')
-    wl('Build directories and files')
+    wl('Build front end directories and files')
 
     print('Installing node dependencies. This will take a while.')
     os.chdir(f('$out', '$'))
