a6218702ad6b5c5b1a940d9be2b0178b08175191
Aaron Price <coding.aaronp@gmail.com>
2017-09-22 22:13:19 -0700

    Move media queries from js to scss

--- assets/components/Navbar.js
+++ assets/components/Navbar.js
@@ -1,3 +1,4 @@
+import { list_user_permission } from '../services/permissions.js'
 import Link from 'next/link'
 import fetch from 'isomorphic-unfetch'
 import React from 'react'
@@ -6,155 +7,124 @@ import Paper from 'material-ui/Paper'
 import Drawer from 'material-ui/Drawer'
 import MenuItem from 'material-ui/MenuItem'
 import Router from 'next/router'
-import isMobile from 'ismobilejs'
 import { Navbar, Nav, NavItem } from 'reactstrap'
 
-// Styles
-let link_style = {
-    marginRight: 15,
-    minHeight: 15,
-    color: '#000000',
-    fontWeight: '300',
-}
-
 // Home
-const HomeLink = (props) => {
-    if (props.isMobile) {
-        return (<MenuItem href='/'>Home</MenuItem>)
-    } else {
-        return (<RaisedButton style={link_style} label="Home" href="/" />)
-    }
+const HomeLinkMobile = (props) => {
+    return <MenuItem className='homelink--mobile' href='/'>Home</MenuItem>
+}
+const HomeLinkDesktop = (props) => {
+    return <RaisedButton
+                className='homelink--desktop menubar__button--link'
+                label="Home"
+                href="/" />
 }
 
 // Content
 const content_types = []
-const ContentLinks = (props) => (
+
+const ContentLinksMobile = (props) => (
     <ul className="navbar-nav mr-auto">
         { content_types.map((item, key) => {
           let has_permission = true
-          if ('permissions' in item) {
-              has_permission = item.permissions(props.current_user)
-          }
+
           if (has_permission) {
               const lower = item.title.toLowerCase()
-              if (props.isMobile) {
                   return (
                       <li key={key}>
                           <MenuItem
                               href={`/${lower}`}
-                              style={link_style}
+                              className='menubar__button--link'
                               >{item.title}</MenuItem>
                       </li>
                   )
-              } else {
-                  return (
-                      <li key={key}>
-                          <RaisedButton
-                              href={`/${lower}`}
-                              style={link_style}
-                              label={item.title} />
-                      </li>
-                  )
-              }
           } else {
             return <span key={key}></span>
           }
         })}
     </ul>
 )
+const ContentLinksDesktop = (props) => (
+    <ul className="navbar-nav mr-auto">
+        { content_types.map((item, key) => {
+              const lower = item.title.toLowerCase()
+              return (
+                  <li key={key}>
+                      <RaisedButton
+                          href={`/${lower}`}
+                          className='menubar__button--link'
+                          label={item.title} />
+                  </li>
+              )
+        )}
+    </ul>
+)
 
-let close_styles = {}
 class NavContainer extends React.Component {
     constructor(props) {
         super(props)
-        this.state = {
-            isOpen: false,
-            mobile_determined: false,
-            isMobile: true
-        }
-        this.toggle = this.toggle.bind(this)
-        this.check_mobile = this.check_mobile.bind(this)
+        this.state = { isOpen: false }
     }
-    check_mobile() {
-        this.setState({
-            isMobile: isMobile.any || window.innerWidth < 960
-        })
-    }
-    componentDidMount() {
-        this.check_mobile()
-        this.setState({ mobile_determined: true })
-        window.addEventListener('resize', this.check_mobile)
-    }
+
     toggle() {
         this.setState({
             isOpen: !this.state.isOpen
         });
     }
+
     render() {
-        let authenticated = !!this.state.id && !!this.state.name
-        if (!this.state.mobile_determined) {
-            return <div style={{backgroundColor:'#0097A7', minHeight: '3.5em'}}></div>
-        } else {
-            return (
-                <div style={{backgroundColor:'#0097A7', minHeight: '3.5em'}}>
-                    <MobileMenubar
-                        toggle={this.toggle}
-                        isOpen={this.state.isOpen}
-                        isMobile={this.state.isMobile} />
-                    <DesktopMenubar isMobile={this.state.isMobile} />
-                </div>
-            )
-        }
+        return (
+          <div className='menubar--wrapper'>
+              <MobileMenubar
+                  toggle={this.toggle}
+                  isOpen={this.state.isOpen} />
+              <DesktopMenubar />
+          </div>
+        )
+
     }
 }
 
 const MobileMenubar = (props) => {
-    if (props.isMobile) {
-        return (
-          <div>
-              <Navbar toggleable>
-                  <Nav className='ml-auto' navbar>
-                      <NavItem>
-                          <RaisedButton
-                              label='Menu'
-                              onClick={props.toggle} />
-                      </NavItem>
-                  </Nav>
-              </Navbar>
-              <Drawer open={props.isOpen}>
-                  <RaisedButton
-                      secondary={true}
-                      label='Close (X)'
-                      style={link_style}
-                      onClick={props.toggle} />
-                  <HomeLink isMobile={props.isMobile} />
-                  <ContentLinks isMobile={props.isMobile} />
-              </Drawer>
-          </div>
-        )
-    } else {
-        return (<span></span>)
-    }
+    return (
+      <div className='menubar--mobile'>
+          <Navbar className='menubar__button--regular' toggleable>
+              <Nav navbar>
+                  <NavItem>
+                      <RaisedButton
+                          label='Menu'
+                          onClick={props.toggle} />
+                  </NavItem>
+              </Nav>
+          </Navbar>
+          <Drawer open={props.isOpen}>
+              <RaisedButton
+                  secondary={true}
+                  label='Close (X)'
+                  className='menubar__button--link'
+                  onClick={props.toggle} />
+              <HomeLinkMobile />
+              <ContentLinksMobile />
+          </Drawer>
+      </div>
+    )
 }
+
 const DesktopMenubar = (props) => {
-    if (!props.isMobile) {
-        return (
-            <div>
-                <Navbar toggleable>
-                    <Nav className='ml-auto' navbar>
-                        <NavItem>
-                            <HomeLink />
-                        </NavItem>
-                        <NavItem>
-                            <ContentLinks />
-                        </NavItem>
-                    </Nav>
-                </Navbar>
-            </div>
-        )
-    } else {
-        return (<span></span>)
-    }
+    return (
+        <div className='menubar--desktop'>
+            <Navbar toggleable>
+                <Nav className='ml-auto' navbar>
+                    <NavItem>
+                        <HomeLinkDesktop />
+                    </NavItem>
+                    <NavItem>
+                        <ContentLinksDesktop />
+                    </NavItem>
+                </Nav>
+            </Navbar>
+        </div>
+    )
 }
 
 export default NavContainer
