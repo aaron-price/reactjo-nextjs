9c24d814d709bdfc5aa68410cbb05530354a7ac8
Aaron Price <coding.aaronp@gmail.com>
2017-09-27 12:57:03 -0700

    Implement signed cookies

--- assets/middleware/res_current_user.js
+++ assets/middleware/res_current_user.js
@@ -1,11 +1,13 @@
 // This middleware simply adds a res.current_user object with id and name.
 function current_user(req, res, next) {
-		let id = req.cookies.reactjo_id
-		let name = req.cookies.reactjo_name
-		let is_staff = req.cookies.reactjo_is_staff === 'true'
-		let is_superuser = req.cookies.reactjo_is_superuser === 'true'
-		let is_active = req.cookies.reactjo_is_active === 'true'
-		let token = req.cookies.reactjo_token
+		let src = req.cookies
+
+		let id 					 = src.reactjo_id ? src.reactjo_id : null
+		let name 				 = src.reactjo_name ? src.reactjo_name : null
+		let is_staff     = src.reactjo_is_staff ? src.reactjo_is_staff === 'true' : false
+		let is_superuser = src.reactjo_is_superuser ? src.reactjo_is_superuser === 'true' : false
+		let is_active    = src.reactjo_is_active ? src.reactjo_is_active === 'true' : false
+		let token 			 = src.reactjo_token ? src.reactjo_token : 'false'
 
 		// If one is null, they both should be.
 		if (!name || !id) {
