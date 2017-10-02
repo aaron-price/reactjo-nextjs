13e2c3c0ca96adabed4e1907f119e5c9f92636f9
Aaron Price <coding.aaronp@gmail.com>
2017-10-02 15:55:35 -0700

    Fix typechecking

--- assets/services/content_update.js
+++ assets/services/content_update.js
@@ -2,7 +2,7 @@ const fetch = require('isomorphic-unfetch')
 const { get_uri } = require('../services/get_uri.js')
 
 const update_content_service = (req, res, app, content_type) => {
-    const type = content_type === 'user' ? 'profile' : content_type
+    const type = content_type === 'profile' ? 'user' : content_type
     const CONTENT_URL = `${get_uri({res}).backend}/api/${type}/${req.body.id}/`
 
     // Body
