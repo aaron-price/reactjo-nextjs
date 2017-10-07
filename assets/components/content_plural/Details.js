213c35ec14950c1b0cda142f77a4976878a2b589
Aaron Price <coding.aaronp@gmail.com>
2017-10-06 20:03:26 -0700

    Fix content details component

--- assets/components/content_plural/Details.js
+++ assets/components/content_plural/Details.js
@@ -3,15 +3,16 @@ const Details = (props) => (
         <h1>{props.singular_lower.string_method}</h1>
         <ul>
             {Object.keys(props.singular_lower).map((field, key) => {
-                if (field === 'owner' || field === 'pk') {
-                    return <span></span>
+              let blacklist = ['owner', 'pk', 'owner_name']
+              if (blacklist.indexOf(field) !== -1) {
+                    return <span key={key}></span>
                 } else {
                     return <li key={key}>{field}: {props.singular_lower[field]}</li>
                 }
             })}
             <li>owner:
                 <a href={`/user/${props.singular_lower.owner}`}>
-                    {props.singular_lower.owner_name}
+                    &nbsp;{props.singular_lower.owner_name}
                 </a>
             </li>
         </ul>
--- assets/components/users/Details.js
+++ assets/components/users/Details.js
@@ -2,9 +2,12 @@ export default (props) => (
     <div>
         <h1>{props.profile.name}</h1>
         {Object.keys(props.form_fields).map((f, key) => {
-            return f === 'name' || f === 'password'
-                ? <span key={key}></span>
-                : <p key={key}>{f}: {props.profile[f]}</p>
+            let blacklist = ['name', 'password']
+            if (blacklist.indexOf(field) !== -1) {
+                return <span key={key}></span>
+            } else {
+                return <p key={key}>{f}: {props.profile[f]}</p>
+            }
         })}
     </div>
 )
