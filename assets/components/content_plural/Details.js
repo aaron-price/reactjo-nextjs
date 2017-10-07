a3769bc5ab8adbd0ad2c95c59ea66e80f8f4feab
Aaron Price <coding.aaronp@gmail.com>
2017-10-06 19:53:33 -0700

    Fix content details component

--- assets/components/content_plural/Details.js
+++ assets/components/content_plural/Details.js
@@ -3,14 +3,14 @@ const Details = (props) => (
         <h1>{props.singular_lower.string_method}</h1>
         <ul>
             {Object.keys(props.singular_lower).map((field, key) => {
-                if (f === 'owner' || f === 'pk') {
+                if (field === 'owner' || field === 'pk') {
                     return <span></span>
                 } else {
                     return <li key={key}>{field}: {props.singular_lower[field]}</li>
                 }
             })}
             <li>owner:
-                <a href={`/user/${props.owner}`}>
+                <a href={`/user/${props.singular_lower.owner}`}>
                     {props.singular_lower.owner_name}
                 </a>
             </li>
