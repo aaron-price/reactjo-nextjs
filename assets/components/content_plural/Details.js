2eaa5bec02a8674da697fee54ba2234b759bd300
Aaron Price <coding.aaronp@gmail.com>
2017-10-06 19:27:57 -0700

    Improve content details component

--- assets/components/content_plural/Details.js
+++ assets/components/content_plural/Details.js
@@ -3,8 +3,17 @@ const Details = (props) => (
         <h1>{props.singular_lower.string_method}</h1>
         <ul>
             {Object.keys(props.singular_lower).map((field, key) => {
-                return <li key={key}>{field}: {props.singular_lower[field]}</li>
+                if (f === 'owner' || f === 'pk') {
+                    return <span></span>
+                } else {
+                    return <li key={key}>{field}: {props.singular_lower[field]}</li>
+                }
             })}
+            <li>owner:
+                <a href={`/user/${props.owner}`}>
+                    {props.singular_lower.owner_name}
+                </a>
+            </li>
         </ul>
     </div>
 )
