31793e2258de0fb2ba2ee85479c7352cd20e8fca
Aaron Price <coding.aaronp@gmail.com>
2017-11-10 14:30:02 -0800

    Update index text

--- assets/pages/index.js
+++ assets/pages/index.js
@@ -12,10 +12,15 @@ import { return_current_user } from '../services/current_user.js'
 export const index = props => (
     <Header current_user={props.current_user}>
         <h1>Reactjo Index</h1>
-        <p>To add a new page, go to /frontent/pages/ and add a new file with a react component. You can use index.js as an example template</p>
-        <p>To add a menu item, go to /frontend/components/Navbar.js</p>
-        <p>To add a new model, open a terminal and type <code>reactjo c</code></p>
-        <p>To adjust permissions, open /frontend/services/permissions.js</p>
+        <p>To add a new page, add a new .js file in /frontend/pages.<br />
+            You can use index.js as an example template.<br />
+            Try to keep all the state and logic in the page file, and all the visual markup in a separate /components/ file.<br />
+            A new route is automatically created for every page</p>
+        <p>To add new custom routes, see /frontend/server.js</p>
+        <p>Add menu items in /frontend/components/Navbar.js</p>
+        <p>To add new content types, open a terminal and type <code>reactjo c</code> or <code>reactjo content</code></p>
+        <p>To adjust permissions, see /frontend/services/permissions.js</p>
+        <p>For an example of working with redux in reactjo, see <a href='/redux_demo'>/pages/redux_demo</a></p>
     </Header>
 )
 
